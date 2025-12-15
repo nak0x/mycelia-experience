ff5a8e3ce3c3e6e9c6f884b433148d7967cb0365
Lonejyc <jocelyn.marcilloux@gmail.com>
2025-12-15 12:53:44 +0100

    feat: inter 4 - eau

---
+++ interaction-2/eau/app/src/controller.py
@@ -0,0 +1,16 @@
+from framework.controller import Controller
+from framework.utils.frames.frame import Frame
+
+class ExampleController(Controller):
+    
+    def setup(self):
+        pass
+
+    def update(self):
+        pass
+
+    def shutdown(self):
+        pass
+
+    def on_frame_received(self, frame: Frame):
+        pass
