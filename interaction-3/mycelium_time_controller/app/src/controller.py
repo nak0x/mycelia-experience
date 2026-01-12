c80b62a0c016fc4fb9b07818c247714ced77a267
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2026-01-12 20:05:41 +0100

    Feat: avoid flood clock

--- interaction-3/mycelium_time_controller/app/src/controller.py
+++ interaction-3/mycelium_time_controller/app/src/controller.py
@@ -2,26 +2,36 @@ from framework.controller import Controller
 from framework.components.encoder import Encoder
 from framework.utils.gpio import GPIO
 from framework.utils.ws.interface import WebsocketInterface
+from framework.utils.timer import Timer
 
 class MainController(Controller):
     def setup(self):
         self.interaction_2_done = False
+        self.ws_authorize = True
+        self.timer = Timer(200, self._authorize_ws)
         Encoder(pinA=GPIO.GPIO25, pinB=GPIO.GPIO26, onCw=self.increment_mycelium, onCcw=self.decrement_mycelium, onChange=self.shake_sphero)
 
-    def shake_sphero(self):
+    def shake_sphero(self, position, step):
         if not self.interaction_2_done:
-            print("> shake sphero")
-            WebsocketInterface().send_value("02-shake-sphero", None)
+            self._send_ws("03-shake-sphero")
 
     def increment_mycelium(self):
         if self.interaction_2_done:
-            print("> grow mycelium")
-            WebsocketInterface().send_value("03-grow-mycelium", None)
+            self._send_ws("03-grow-mycelium")
 
     def decrement_mycelium(self):
         if self.interaction_2_done:
-            print("> shrink mycelium")
-            WebsocketInterface().send_value("03-shrink-mycelium", None)
+            self._send_ws("03-shrink-mycelium")
+
+    def _send_ws(self, action):
+        if self.ws_authorize:
+            print(f"> {action}")
+            WebsocketInterface().send_value(action, None)
+            self.ws_authorize = False
+            self.timer.restart()
+
+    def _authorize_ws(self):
+        self.ws_authorize = True
 
     def on_frame_received(self, frame):
         if frame.action == "02-interaction-done" or frame.action == "03-interaction-start":
