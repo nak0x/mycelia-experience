741c849096ce9851637597b18714579df41469ae
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2026-01-12 17:45:29 +0100

    Feat: multi interaction clock

--- interaction-3/mycelium_time_controller/app/src/controller.py
+++ interaction-3/mycelium_time_controller/app/src/controller.py
@@ -5,8 +5,26 @@ from framework.utils.ws.interface import WebsocketInterface
 
 class MainController(Controller):
     def setup(self):
-        Encoder(pinA=GPIO.GPIO25, pinB=GPIO.GPIO26, onCw=self.increment_mycelium)
+        self.interaction_2_done = False
+        Encoder(pinA=GPIO.GPIO25, pinB=GPIO.GPIO26, onCw=self.increment_mycelium, onCcw=self.decrement_mycelium, onChange=self.shake_sphero)
+
+    def shake_sphero(self):
+        if not self.interaction_2_done:
+            print("> shake sphero")
+            WebsocketInterface().send_value("02-shake-sphero", None)
 
     def increment_mycelium(self):
-        print("> grow mycelium")
-        WebsocketInterface().send_value("03-grow-mycelium", None)
+        if self.interaction_2_done:
+            print("> grow mycelium")
+            WebsocketInterface().send_value("03-grow-mycelium", None)
+
+    def decrement_mycelium(self):
+        if self.interaction_2_done:
+            print("> shrink mycelium")
+            WebsocketInterface().send_value("03-shrink-mycelium", None)
+
+    def on_frame_received(self, frame):
+        if frame.action == "02-interaction-done" or frame.action == "03-interaction-start":
+            self.interaction_2_done = True
+        elif frame.action == "02-reset":
+            self.interaction_2_done = False
