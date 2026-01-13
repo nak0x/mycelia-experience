57d6472fcce90a582e169cbdf93141758d9917ca
Nak0_x <nak0x@proton.me>
2026-01-13 02:00:17 +0100

    Wip: rework shrooms

--- interaction-1/shrooms/app/src/controller.py
+++ interaction-1/shrooms/app/src/controller.py
@@ -1,8 +1,12 @@
 from framework.controller import Controller
+from framework.components.led_strip import LedStrip
+from framework.components.mcp3008 import MCP3008
 
 from .shrooms.shrooms_controller import ShroomsController
 
 class MainController(Controller):
     def __init__(self):
         super().__init__()
-        self.shrooms = ShroomsController(leds_pin=27, leds=136, shrooms=20, shroom_len=3, gap=7, color=(229, 141,18), aux_leds_pin=26,aux_leds=250)
+        leds = LedStrip(27, 300, (224, 156, 24))
+        mcp = MCP3008()
+        self.shrooms = ShroomsController(leds, mcp)
