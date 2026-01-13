0f9b75f476916012bbe128a94f2203186caad274
Nak0_x <nak0x@proton.me>
2026-01-13 01:26:28 +0100

    Upt: Update framework to embarque mcp

--- interaction-1/wind-turbine/app/framework/components/relay.py
+++ interaction-1/wind-turbine/app/framework/components/relay.py
@@ -5,11 +5,8 @@ from framework.utils.frames.frame import Frame
 class Relay:
     is_open = False
 
-    def __init__(self, pin, action = None, on_payload_received = None):
-        self.pin = Pin(pin, Pin.OUT)
-        self.action = action
-        self.on_payload_received_callback = on_payload_received
-        App().on_frame_received.append(self.on_frame_received)
+    def __init__(self, pin):
+        self.pin = Pin(pin, Pin.OUT, value=1)
 
     def open(self):
         self.pin.value(0)
@@ -24,13 +21,3 @@ class Relay:
             self.close()
         else:
             self.open()
-
-    def on_frame_received(self, frame: Frame):
-        print(f"Relay : {frame} \n action: {self.action}")
-        if self.action != frame.action:
-            return
-
-        if self.on_payload_received_callback is not None:
-            self.on_payload_received_callback(self, frame.value)
-        elif isinstance(frame.value, bool):
-            self.close() if frame.value else self.open()
