13be360b76dd7982237dc85107702aeb7506abd7
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2026-01-16 20:53:38 +0100

    Upt: framework led_strip

--- devkit/python-esp32-template/app/framework/components/led_strip.py
+++ devkit/python-esp32-template/app/framework/components/led_strip.py
@@ -47,28 +47,29 @@ class LedStrip:
     def __init__(self, pin, pixel_num, max_current=2, default_color=(255, 255, 255)):
         self.pixel_num = pixel_num
         self.np = NeoPixel(Pin(pin), pixel_num)
+        self.max_current = max_current
         self.default_color = scale_rgb_for_power(max_current, pixel_num, default_color)
 
         self.pixels = [(0, 0, 0)] * pixel_num
         self.display()
+
+    def scale_color(self, color):
+        return scale_rgb_for_power(self.max_current, len(self.pixels), color) if color is not None else None
 
     def display(self):
         for i, c in enumerate(self.pixels):
             self.np[i] = c
         self.np.write()
 
-    def fill(self, color):
+    def fill(self, color=None):
         for i in range(len(self.pixels)):
-            self.pixels[i] = scale_rgb_for_power(2, len(self.pixels), color) or self.default_color
+            self.pixels[i] = self.scale_color(color) or self.default_color
 
     def clear(self):
         self.fill((0, 0, 0))
 
     def on(self, color=None):
-        if color is None:
-            self.fill(self.default_color)
-        else:
-            self.fill(scale_rgb_for_power(2, len(self.pixels), color))
+        self.fill(color)
         self.display()
 
     def off(self):
@@ -77,42 +78,31 @@ class LedStrip:
 
     def set_pixel(self, i, color=None, show=False):
         if 0 <= i < len(self.pixels):
-            if color is None:
-                self.pixels[i] = self.default_color
-            else:
-                self.pixels[i] = scale_rgb_for_power(2, len(self.pixels), color)
+            self.pixels[i] = self.scale_color(color) or self.default_color
             if show:
                 self.display()
 
-    def _last_index_of_color(self, color):
+    def _last_index_of_color(self, color=None):
         for i in range(len(self.pixels) - 1, -1, -1):
-            if self.pixels[i] == scale_rgb_for_power(2, len(self.pixels), color):
+            if self.pixels[i] == (self.scale_color(color) or self.default_color):
                 return i
         return None
     
     def next_pixel(self, color=None, show=False):
-        last = self._last_index_of_color(color or self.default_color)
-        if color is None:
-            c = self.default_color
-        else: 
-            c = scale_rgb_for_power(2, len(self.pixels), color)
+        last = self._last_index_of_color(color)
 
         i = 0 if last is None else min(last + 1, len(self.pixels) - 1)
-        self.pixels[i] = c
+        self.set_pixel(i, color)
 
         if show:
             self.display()
         return i
 
     def previous_pixel(self, color=None, show=False):
-        last = self._last_index_of_color(color or self.default_color)
-        if color is None:
-            c = self.default_color
-        else: 
-            c = scale_rgb_for_power(2, len(self.pixels), color)
-
+        last = self._last_index_of_color(color)
+        
         i = (len(self.pixels) - 1) if last is None else max(last - 1, 0)
-        self.pixels[i] = c
+        self.set_pixel(i, color)
 
         if show:
             self.display()
