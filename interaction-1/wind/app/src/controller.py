a6e707fcc41b84f3f8c9321e1adabaedc6894051
Emmanuel Moulin <78955048+Kibishi47@users.noreply.github.com>
2026-01-19 15:14:40 +0100

    Feat: reset wind on reset

--- interaction-1/wind/app/src/controller.py
+++ interaction-1/wind/app/src/controller.py
@@ -4,6 +4,11 @@ from framework.utils.gpio import GPIO
 
 class WindController(Controller):
     
-    def __init__(self):
-        super().__init__()
-        Relay(GPIO.GPIO27, "01-wind-toggle")
+    def setup(self):
+        self.relay = Relay(GPIO.GPIO27)
+    
+    def on_frame_received(self, frame):
+        if frame.action == "01-wind-toggle":
+            self.relay.open() if frame.value == True else self.relay.close()
+        elif frame.action == "01-reset":
+            self.relay.close()
